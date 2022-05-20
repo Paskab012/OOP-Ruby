359418ee7d5b82bbb58503f98fab986dd8d0f1b2
Pascal <pascal@Pascals-MacBook-Pro.local>
2022-05-20 16:18:48 +0200

    Correct linters errors by rubocop

--- book.rb
+++ book.rb
@@ -1,14 +1,13 @@
-class Book 
-    def initialize(title, author)
-        @title = title,
-        @author = author
-        @rentals = []
-    end
+class Book
+  def initialize(title, author)
+    @title = title,
+             @author = author
+    @rentals = []
+  end
 
-    attr_accessor :title, :author, :rentals
+  attr_accessor :title, :author, :rentals
 
-    def add_rental(person, date)
-        Rental.new(date, self, person)
-    end
+  def add_rental(person, date)
+    Rental.new(date, self, person)
+  end
 end
-    
--- classroom.rb
+++ classroom.rb
@@ -1,13 +1,13 @@
-class Classroom 
-    def initialize(label)
-        @label = label
-        @students = []
-    end
+class Classroom
+  def initialize(label)
+    @label = label
+    @students = []
+  end
 
-    attr_accessor :label, :students
+  attr_accessor :label, :students
 
-    def add_student(student)
-        @students.push(student)
-        student.classroom = self
-      end
-    end
+  def add_student(student)
+    @students.push(student)
+    student.classroom = self
+  end
+end
